e5b89f3b9d90af9b5b804789c0955d427f2353ea
Vianne Schwaebe <Vivicoolcat1@users.noreply.github.com>
2021-12-09 12:52:47 -0800

    Add files via upload

---
+++ Q2 Project2-Daniela-Vianne/src/Table.java
@@ -0,0 +1,68 @@
+
+
+	import java.awt.Graphics;
+	import java.awt.Graphics2D;
+	import java.awt.Image;
+	import java.awt.Rectangle;
+	import java.awt.Toolkit;
+	import java.awt.event.MouseEvent;
+	import java.awt.event.MouseListener;
+	import java.awt.geom.AffineTransform;
+	import java.net.URL;
+	import java.util.Random;
+	public class Table {
+	 private int x;
+	 private int y;
+	 private Image img; 
+
+		private AffineTransform tx;
+	public Table(int x, int y) {
+		this.x=x;
+		this.y=y;
+		img = getImage("/imgs/table.png"); //load the image for Tree
+	
+		
+		tx = AffineTransform.getTranslateInstance(x, y );
+			init(x, y); 
+	}
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		
+		//call update to update the actualy picture location
+		update();
+		
+		
+		
+		
+		g2.drawImage(img, tx, null);
+		
+		
+
+	}
+	/* update the picture variable location */
+	private void update() {
+
+	tx.setToTranslation(x,y);
+	tx.scale(.5,.1);	
+	
+	}
+
+
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(.5,.1);
+	}
+
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+}
