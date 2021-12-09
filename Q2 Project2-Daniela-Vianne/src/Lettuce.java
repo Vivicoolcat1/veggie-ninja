7fcdd15040a886853377d1add9b664184d425dfa
Vianne Schwaebe <Vivicoolcat1@users.noreply.github.com>
2021-12-09 12:50:43 -0800

    Update Lettuce.java

--- Q2 Project2-Daniela-Vianne/src/Lettuce.java
+++ Q2 Project2-Daniela-Vianne/src/Lettuce.java
@@ -9,14 +9,19 @@ import java.awt.Toolkit;
 	import java.awt.event.MouseListener;
 	import java.awt.geom.AffineTransform;
 	import java.net.URL;
+import java.util.Random;
 	public class Lettuce {
 	 private int x;
 	 private int y;
+	 private Random r;
+		private int respX;
 	 private Image img; 
 		private AffineTransform tx;
 	public Lettuce(int x, int y) {
 		this.x=x;
 		this.y=y;
+		r=new Random();
+		respX=r.nextInt(((650-10)+1)+10);
 		img = getImage("/imgs/Lettuce.png"); //load the image for Tree
 		tx = AffineTransform.getTranslateInstance(x, y );
 			init(x, y); 
@@ -40,8 +45,8 @@ import java.awt.Toolkit;
 	}
 	/* update the picture variable location */
 	private void update() {
-
-		y++;
+		respX=r.nextInt(((650-10)+1)+10);
+		y+=4;
 		tx.setToTranslation(x, y);
 		tx.scale(.3,.3);
 	}
@@ -70,6 +75,11 @@ import java.awt.Toolkit;
 		changePicture("/imgs/slicedCabbege.png");
 		
 		}
+	public void respawn() {
+		changePicture("/imgs/Lettuce.png");
+		y=-100;
+		x=respX;
+	}
 	public Rectangle getRect() {
 		return new Rectangle(x, y, 120, 100);
 	}
