a6e9b3c045d7450a899028a93aaebc17558a10fa
Vianne Schwaebe <Vivicoolcat1@users.noreply.github.com>
2021-12-09 12:47:26 -0800

    Update Background.java

--- Q2 Project2-Daniela-Vianne/src/Background.java
+++ Q2 Project2-Daniela-Vianne/src/Background.java
@@ -1,54 +1,85 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.geom.AffineTransform;
+import java.awt.event.MouseMotionListener;
 import java.net.URL;
 
-public class Background{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener, MouseMotionListener {
+	int mouseY = MouseInfo.getPointerInfo().getLocation().y; 
+	int mouseX = MouseInfo.getPointerInfo().getLocation().x;
+	//CREATE THE OBJECT (STEP 1)
+	Background 	bg 	= new Background(0, -100);
 	
-	//add location attributes
-	private Image img; 	
-	private AffineTransform tx;
+Broccoli b= new Broccoli(100,200);
+Lettuce l =new Lettuce(200,300);
+Corn c = new Corn( 100, 100);
+Onion o = new Onion(280,100);
+Knife sword= new Knife(250,490);
+Potato p= new Potato(400, 300);
+Carrot ca= new Carrot(300, 400);
+Table t=new Table(-150,450);
+boolean bisDead=false;
+boolean oisDead=false;
+boolean cisDead=false;
+boolean lisDead=false;
+boolean pisDead=false;
+boolean caisDead=false;
+ Image img =getImage("/imgs/x.png");
 
-	public Background(int x, int y) {
-		img = getImage("/imgs/background.png"); //load the image for Tree
-		tx = AffineTransform.getTranslateInstance(x, y );
-		init(x, y); 				//initialize the location of the image
-									//use your variables
-	}
+public void paint(Graphics g) {
+		super.paintComponent(g);
+		bg.paint(g);
+b.paint(g);
+l.paint(g);
+c.paint(g);
+o.paint(g);
+sword.paint(g);
+p.paint(g);
+ca.paint(g);
+t.paint(g);
+collide();
 
+if(bisDead==false && b.getY()>580) {
+	g.drawImage(img, 200, 100, 250,100, 250, 200, 200, 200, null);
+}
+}
 	
-	public void paint(Graphics g) {
-		//these are the 2 lines of code needed draw an image on the screen
-		Graphics2D g2 = (Graphics2D) g;
-		
-		
-		//call update to update the actualy picture location
-		update();
-		
-		
-		
-		
-		g2.drawImage(img, tx, null);
-		
-		
-
-	}
-	/* update the picture variable location */
-	private void update() {
-
-		
+	public static void main(String[] arg) {
+		Frame f = new Frame();
 		
 	}
 	
-	private void init(double a, double b) {
-		tx.setToTranslation(a, b);
-		tx.scale(.8,1.2);
+	public Frame() {
+		JFrame f = new JFrame("Vegtable Slice");
+		f.setSize(new Dimension(700, 600));
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addMouseMotionListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
 	}
-
 	private Image getImage(String path) {
 		Image tempImage = null;
 		try {
@@ -58,6 +89,122 @@ public class Background{
 			e.printStackTrace();
 		}
 		return tempImage;
+	}
+	public void collide() {
+		if(b.getRect().intersects(sword.getRect())) {
+			b.die();
+			bisDead=true;
+			if(lisDead==true) {
+				l.respawn();
+			}
+		
+		}
+	    if(l.getRect().intersects(sword.getRect())) {
+			l.die();
+			lisDead=true;
+			if(caisDead==true) {
+				ca.respawn();
+			}
+		}
+		if(ca.getRect().intersects(sword.getRect())) {
+			ca.die();
+			caisDead=true;
+			if(cisDead==true) {
+				c.respawn();
+			}
+		}
+		if(c.getRect().intersects(sword.getRect())) {
+			c.die();
+			cisDead=true;
+			if(oisDead==true) {
+				o.respawn();
+			}
+		}
+		if(o.getRect().intersects(sword.getRect())) {
+			o.die();
+			oisDead=true;
+			if(pisDead==true) {
+				p.respawn();
+			}
+		}
+		if(p.getRect().intersects(sword.getRect())) {
+			p.die();
+			pisDead=true;
+			if(bisDead==true) {
+				b.respawn();
+			}
+		}
+		
+	}
+	
+	
+	/*public void Start() {
+		sword.setX(mouseX);
+		sword.setY(mouseY); 
+	}*/ 
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		 
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		 
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		System.out.println(arg0.getKeyCode());
+
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		sword.setPositioin(e.getX(), e.getY());
 	}
 
 }
