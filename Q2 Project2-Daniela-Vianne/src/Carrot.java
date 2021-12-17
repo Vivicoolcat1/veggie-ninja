231a0cd1e43fcb1b384dbc96a136cac009b1b585
DanielaFisher23 <78383220+DanielaFisher23@users.noreply.github.com>
2021-12-17 13:22:12 -0800

    Update Carrot.java

--- Q2 Project2-Daniela-Vianne/src/Carrot.java
+++ Q2 Project2-Daniela-Vianne/src/Carrot.java
@@ -46,7 +46,7 @@ import java.util.Random;
 	/* update the picture variable location */
 	private void update() {
 		respX=r.nextInt(((650-10)+1)+10);
-		y+=4;
+		y+=6;
 		tx.setToTranslation(x, y);
 		tx.scale(.35,.35);
 
