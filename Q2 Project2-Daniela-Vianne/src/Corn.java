598f2bf30ce9943d8748314050d19b350d0298fc
DanielaFisher23 <78383220+DanielaFisher23@users.noreply.github.com>
2021-12-17 13:22:27 -0800

    Update Corn.java

--- Q2 Project2-Daniela-Vianne/src/Corn.java
+++ Q2 Project2-Daniela-Vianne/src/Corn.java
@@ -45,7 +45,7 @@ import java.util.Random;
 	/* update the picture variable location */
 	private void update() {
 		respX=r.nextInt(((650-10)+1)+10);
-		y+=4;
+		y+=3;
 		
 		tx.setToTranslation(x, y);
 		tx.scale(.04,.04);
