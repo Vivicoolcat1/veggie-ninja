33fc1d2d86f5c3c6364cabfdeed6eab104fa91ca
Vianne Schwaebe <Vivicoolcat1@users.noreply.github.com>
2022-01-05 13:47:43 -0800

    Update Lettuce.java

--- Q2 Project2-Daniela-Vianne/src/Lettuce.java
+++ Q2 Project2-Daniela-Vianne/src/Lettuce.java
@@ -14,34 +14,42 @@ import java.util.Random;
 	 private int x;
 	 private int y;
 	 private Random r;
+	 private boolean isAlive;
+	 private boolean hasBeenHit;
 		private int respX;
 	 private Image img; 
 		private AffineTransform tx;
-	public Lettuce(int x, int y) {
+	public Lettuce(int x, int y,boolean isAlive, boolean hasBeenHit) {
 		this.x=x;
 		this.y=y;
+		this.isAlive=isAlive;
+		this.hasBeenHit=hasBeenHit;
 		r=new Random();
 		respX=r.nextInt(((650-10)+1)+10);
 		img = getImage("/imgs/Lettuce.png"); //load the image for Tree
 		tx = AffineTransform.getTranslateInstance(x, y );
-			init(x, y); 		
+			init(x, y); 
+			
 	}
-	
 	public void paint(Graphics g) {
 		//these are the 2 lines of code needed draw an image on the screen
 		Graphics2D g2 = (Graphics2D) g;
 		
+		
 		//call update to update the actualy picture location
 		update();
-			
+		
+		
+		
+		
 		g2.drawImage(img, tx, null);
-		g.drawRect(x, y, 120, 100);	
+		
+		
 
 	}
-	
 	/* update the picture variable location */
 	private void update() {
-		respX=r.nextInt(((650-10)+1)+10); // randomizing x value 
+		respX=r.nextInt(((650-10)+1)+10); //randomizing x value 
 		y+=2; //falling down 
 		tx.setToTranslation(x, y);
 		tx.scale(.3,.3);
@@ -62,35 +70,34 @@ import java.util.Random;
 		}
 		return tempImage;
 	}
-	
 	public void changePicture(String newFileName) {
 		img = getImage(newFileName);
 		init(x, y);
 		
 	}
-	
-	//changing image after collision
+		
+	//changing picture after collision 
 	public void die() {
 		changePicture("/imgs/slicedCabbege.png");
 		
 		}
-	
-	public void respawn() {
-		changePicture("/imgs/Lettuce.png");
-		y=-100;
-		x=respX;
-	}
-	
+
 	public Rectangle getRect() {
 		return new Rectangle(x, y, 120, 100);
 	}
 
-//getters and setters
+//getters and setters 
 	public int getX() {
 		return x;
 	}
 	public int getY() {
 		return y;
+	}
+	public boolean getHit() {
+		return hasBeenHit;
+	}
+	public boolean getAlive() {
+		return isAlive;
 	}
 	public void setX(int val) {
 		x=val;
@@ -98,4 +105,11 @@ import java.util.Random;
 	public void setY(int val) {
 		y=val;
 	}
+	public void setDead(boolean sat) {
+		isAlive=sat;
 	}
+	public void setHit(boolean stat) {
+		hasBeenHit=stat;
+	}
+	}
+
